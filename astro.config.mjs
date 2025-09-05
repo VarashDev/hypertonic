6d211301e8f974259065034e7b353cfcbd47dd8e
VarashDev <varashdev@gmail.com>
2025-09-05 18:40:58 +0300

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,21 @@ import netlify from '@astrojs/netlify'
 import clerk from '@clerk/astro'
 import { ukUA } from '@clerk/localizations'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   output: 'server',
+
   integrations: [clerk({
     localization: ukUA,
   })],
+
   adapter: netlify({
     // Important for Clerk on Netlify: leave edgeMiddleware OFF
     // edgeMiddleware: false // (default)
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
